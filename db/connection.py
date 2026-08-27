3fa928fae4d177ca61d615617654417716c95154
@GitHub2024 <mathfer1000@gmail.com>
2026-08-26 22:02:12 -0300

    conn(mongo-db): adicionar módulo principal .py de conexão com o banco de dados

--- db/connection.py
+++ db/connection.py
@@ -1,8 +1,22 @@
-import os
+from os import environ
+from fastapi import FastAPI
+from dotenv import dotenv_values
 from pymongo import MongoClient
 
 # Lê a URL configurada no docker-compose.yml
-mongo_uri = os.environ["MONGO_URI"]
-client = MongoClient(mongo_uri)
+mongo_uri = environ["MONGO_URI"]
 
-db = client["meubanco"]
+config = dotenv_values(".env")
+app = FastAPI()
+
+
+@app.on_event("startup")
+def iniciar_cliente_db():
+    app.mongodb_client = MongoClient(mongo_uri)
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connected to the MongoDB database!")
+
+
+@app.on_event("shutdown")
+def finalizar_cliente_db():
+    app.mongodb_client.close()
